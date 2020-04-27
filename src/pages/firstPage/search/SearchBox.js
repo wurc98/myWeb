9893c516e1030464ff4fd656858eddf9356b18f2
wurc <wurc@sailwish.com>
2020-04-27 15:40:58 +0800

    全局搜索

--- src/pages/firstPage/search/SearchBox.js
+++ src/pages/firstPage/search/SearchBox.js
@@ -3,21 +3,34 @@ import styles from './SearchBox.css';
 import { Input } from 'antd';
 import "antd/dist/antd.css";
 import logo from '../../../assets/img/logo.png'
-export default function SearchBox() {
-  const { Search } = Input;
-  return (
-    <div className={styles.normal}>
-      <div className={styles.logo}>
-          <img src={logo} />
-          <div className={styles.search}>
-            <Search
-              placeholder="请输入..."
-              enterButton="搜索"
-              size="large"
-              onSearch={value => console.log(value)}
-            />
-          </div>
+import React,{Component} from "react"
+import router from "umi/router"
+
+
+class SearchBox extends Component{
+  constructor(props){
+    super(props)
+  }
+  search=value=>{
+    router.push("/products/"+value)
+  }
+  render(){
+    const { Search } = Input;
+    return (
+      <div className={styles.normal}>
+        <div className={styles.logo}>
+            <img src={logo} />
+            <div className={styles.search}>
+              <Search
+                placeholder="请输入..."
+                enterButton="搜索"
+                size="large"
+                onSearch={this.search}
+              />
+            </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+export default SearchBox
