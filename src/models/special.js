cb067e2436e814bae45dada82d0abed96472aebf
wurc <wurc@sailwish.com>
2020-04-30 17:42:38 +0800

    特价

---
+++ src/models/special.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { message } from 'antd'
+//维护页面状态的对象
+
+//api请求  接口调用
+
+function special() {
+    return axios.post('http://localhost:7001/products/special')
+}
+export default {
+    namespace: "special",//命名空间,可省略。省略后，文件名为命名空间。
+    state: [],
+    effects: {//异步操作
+        *special(action, { put, call }){
+            const res = yield call(special)
+            console.log(res)
+            if (res.data.code == 1) {
+                yield put({ type: 'initSpecial', payload: res.data.data })
+            }
+        }
+    },
+    reducers: {
+        initSpecial(state,action){
+          console.log((1111))
+            return action.payload
+        }
+    },
+
+}
--- src/pages/firstPage/special/Special.js
+++ src/pages/firstPage/special/Special.js
@@ -1,102 +1,153 @@
-
 import styles from './Special.css';
+import React, {
+  useEffect
+} from "react"
 import Link from 'umi/link'
-export default function () {
+import {
+  connect
+} from "dva"
 
-  const products = [
-    {
+const mapStateToProps = (state) => {
+  return {
+    specialList: state["special"]
+  }
+}
+
+
+function Special(props) {
+  useEffect(() => {
+    props.dispatch({
+      type: "special/special"
+    }).then(() => {
+      console.log(props.specialList)
+    })
+  })
+
+  const products = [{
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
     {
       img: "http://img3m6.ddimg.cn/61/33/1009943116-1_l_25.jpg",
       info: "人间失格 全本无删减 太宰治绝望的告白 收录 人间失格 斜阳 及绝笔之作 日本经典文学小说 现当代文",
       price: "1",
-      name:"人间失格"
+      name: "人间失格"
     },
   ]
 
-  return (
-    <div className={styles.normal}>
-      <div className={styles.title}>
-        <h3 id="special">特价商品</h3>
-      </div>
-      <div className={styles.specialPro}>
-        {products.map(el => {
-          return (
-            <div className={styles.bookBox}>
-              <Link to={`/products/`+el.name} className={styles.boxImg}>
-                <img src={el.img} />
-                <p>{el.name}</p>
-              </Link>
-              <p>{el.info}</p>
-          <div className={styles.price}>{"特价 ￥："+el.price}</div>
-            </div>
-          )
-        })}
-      </div>
-    </div>
+  return ( <
+    div className = {
+      styles.normal
+    } >
+    <
+    div className = {
+      styles.title
+    } >
+    <
+    h3 id = "special" > 特价商品 < /h3> < /
+    div > <
+    div className = {
+      styles.specialPro
+    } > {
+      products.map(el => {
+        return ( <
+          div className = {
+            styles.bookBox
+          } >
+          <
+          Link to = {
+            `/products/` + el.name
+          }
+          className = {
+            styles.boxImg
+          } >
+          <
+          img src = {
+            el.img
+          }
+          /> <
+          p > {
+            el.name
+          } < /p> < /
+          Link > <
+          p > {
+            el.info
+          } < /p> <
+          div className = {
+            styles.price
+          } > {
+            "特价 ￥：" + el.price
+          } < /div> < /
+          div >
+        )
+      })
+    } <
+    /div> < /
+    div >
   );
 }
+
+
+export default connect(mapStateToProps)(Special)
